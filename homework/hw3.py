8663d2b8de5d88ae878a6c97ab030035a96599a7
Jaidon Lybbert <jaidonlybbert@gmail.com>
2024-04-16 19:03:08 -0700

    Added homework 3

--- homework/hw3.py
+++ homework/hw3.py
@@ -31,7 +31,9 @@ def dtft(x, fs=1, t0=0):
     return lambda f: np.sum(x*np.exp(-2j*np.pi*f/fs*(np.arange(len(x))-t0)))
 
 
-def a():
+def main():
+    # Part A: Generate sampled sinusoid xn from sampling
+    #   x(t) = 2 * cos(2 * f0 * t + phi0) + 2 * cos(2 * pi * f1 + phi0)
     fs = 2E3  # Hz
     f0 = 50   # Hz
     f1 = 150  # Hz
@@ -45,50 +47,47 @@ def a():
         2 * np.cos(2 * np.pi * f1 * t + phi1)
 
     plt.stem(n[:50], xn[:50])
+    plt.xlabel("Sample (n)")
+    plt.ylabel("Magnitude")
+    plt.title("x(t) = 2 * cos(2 * f0 * t + phi0) + 2 * cos(2 * pi * f1 + phi0)")
     plt.show()
 
+    # Part B: Use pydsm.ft.dtft to estimate Fourier transform xw of xn for -pi <= w < pi 
     ws = np.linspace(-np.pi, np.pi, len(n))
+    # pydsm.ft.dtft returns a function of frequency in hertz (not radians)
+    f_arr = ws * fs / (2 * np.pi)
     fft_func = dtft(xn, fs=fs)
-    xw = np.array([fft_func(w) for w in ws])
+    xw = np.array([fft_func(f) for f in f_arr])
 
     # Plotting code copied directly from scipy documentation example
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.freqz.html
     fig, ax1 = plt.subplots()
-    ax1.set_title('Digital filter frequency response')
+    ax1.set_title('DTFT X(w)')
     ax1.plot(ws, np.abs(xw), 'b')  # 20 * np.log10(abs(xw)), 'b')
-    ax1.set_ylabel('Amplitude [dB]', color='b')
-    ax1.set_xlabel('Frequency [radians]')
-
-    ax2 = ax1.twinx()
-    angles = np.unwrap(np.angle(xw))
-    ax2.plot(ws, angles, 'g')
-    ax2.set_ylabel('Angle (radians)', color='g')
-    ax2.grid(True)
-    ax2.axis('tight')
+    ax1.set_ylabel('Magnitude', color='b')
+    ax1.set_xlabel('Frequency [radians/sample]')
     plt.show()
 
     # Part C
     x1sn = np.array([xn[4 * k] for k in np.arange(500)])
     plt.stem(np.arange(50), x1sn[:50])
+    plt.title("x(4n)")
+    plt.xlabel("Sample (n)")
+    plt.ylabel("Magnitude")
     plt.show()
 
-    fft_func = dtft(x1sn, fs=fs/4)  # ??????????
-    xw1 = np.array([fft_func(w) for w in ws])
+    fs = fs/4
+    f_arr = ws * fs / (2 * np.pi)
+    fft_func = dtft(x1sn, fs=fs)
+    xw1 = np.array([fft_func(f) for f in f_arr])
 
     fig, ax1 = plt.subplots()
-    ax1.set_title('Digital filter frequency response')
+    ax1.set_title('DTFT X(w)')
     ax1.plot(ws, np.abs(xw1), 'b')  # 20 * np.log10(abs(xw)), 'b')
-    ax1.set_ylabel('Amplitude [dB]', color='b')
-    ax1.set_xlabel('Frequency [radians]')
-
-    ax2 = ax1.twinx()
-    angles = np.unwrap(np.angle(xw1))
-    ax2.plot(ws, angles, 'g')
-    ax2.set_ylabel('Angle (radians)', color='g')
-    ax2.grid(True)
-    ax2.axis('tight')
+    ax1.set_ylabel('Magnitude', color='b')
+    ax1.set_xlabel('Frequency [radians/sample]')
     plt.show()
 
 
 if __name__ == "__main__":
-    a()
+    main()
